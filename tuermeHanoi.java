281097db7e447e49d99aeefb11e8bded5a1eaebc
Mark <95907950+MarkKrings@users.noreply.github.com>
2022-07-05 17:09:47 +0200

    Add files via upload

---
+++ tuermeHanoi.java
@@ -0,0 +1,17 @@
+public class tuermeHanoi {
+
+    public static void bewegen(char a, char b, char c, int n) {
+        if (n == 1) {
+            System.out.println("Lege die Scheibe von Turm " + a + " auf Turm " + c + ".");
+        } else {
+            bewegen(a, c, b, n-1);          //Rekursion
+            bewegen(a, b, c, 1);
+            bewegen(b, a , c , n-1);
+        }
+    }
+
+    public static void main(String[] args) {
+
+        bewegen('a', 'b','c',3);
+    }
+}
